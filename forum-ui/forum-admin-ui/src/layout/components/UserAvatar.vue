8543c9c54b2ac6050ea370e7f8e38b7aa851fa5c
modakai <2049202552@qq.com>
2024-07-12 22:53:50 +0800

    feat(layout): 新增userAvatar组件

---
+++ forum-ui/forum-admin-ui/src/layout/components/UserAvatar.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+const errorHandler = () => true
+
+const dropdownList = [
+  {
+    label: '个人中心',
+    icon: 'user',
+    path: '/user/center'
+  },
+  {
+    label: '退出登录',
+    icon: 'logout',
+    path: '/login'
+  }
+]
+</script>
+
+<template>
+  <el-dropdown class="avatar" style="line-height: normal">
+    <div class="cursor-pointer mr-16 flex" style="align-items: center">
+      <el-avatar :size="36" src="@/src/assets/images/img.png" @error="errorHandler" />
+      <div class="userInfo">
+        <span>超级管理员</span>
+        <span>[超级管理员]</span>
+      </div>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item v-for="(item, index) in dropdownList" :key="index">
+          {{ item.label }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<style lang="scss" scoped>
+.userInfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-shrink: 0;
+  margin-left: 12px;
+
+  span {
+    &:first-child {
+      font-size: 14px;
+    }
+
+    &:last-child {
+      font-size: 12px;
+    }
+  }
+}
+</style>
--- forum-ui/forum-admin-ui/src/layout/normal/header/index.vue
+++ forum-ui/forum-admin-ui/src/layout/normal/header/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts" name="Header" setup>
 import MenuCollapse from '@/layout/components/MenuCollapse.vue'
 import AppTab from '@/layout/components/AppTab.vue'
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import UserAvatar from '@/layout/components/UserAvatar.vue'
 </script>
 
 <template>
@@ -11,7 +13,16 @@ import AppTab from '@/layout/components/AppTab.vue'
 
     <span class="mx-6 opacity-20">|</span>
 
-    <div style="width: 325px"></div>
+    <div class="flex header-right px-12">
+      <!--  主题切换图标：light dark    -->
+      <svg-icon class="mr-16 cursor-pointer" icon-class="sun" />
+
+      <!-- 全屏图标 -->
+      <svg-icon class="mr-16 cursor-pointer" icon-class="maximize" />
+
+      <!-- 头像 -->
+      <user-avatar />
+    </div>
   </div>
 </template>
 
@@ -23,5 +34,12 @@ import AppTab from '@/layout/components/AppTab.vue'
   width: 100%;
   height: 60px;
   background-color: var(--el-bg-color);
+
+  .header-right {
+    height: 42px;
+    align-items: center;
+    font-size: 18px;
+    flex-shrink: 0;
+  }
 }
 </style>
